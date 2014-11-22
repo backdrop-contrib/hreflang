95068c7e9642495819e1144e28c9850a6dd5ddbc
mark burdett <mfburdett@gmail.com>
2014-11-22 14:58:10 -0800

    Initial d8 version.

---
+++ src/Tests/HreflangTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ * Definition of Drupal\language\Tests\LanguageConfigurationTest.
+ */
+
+namespace Drupal\hreflang\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests for presence of the hreflang link element.
+ *
+ * @group Alternate hreflang
+ */
+class HreflangTest extends WebTestBase {
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('hreflang', 'language');
+
+  /**
+   * Functional tests for the hreflang tag.
+   */
+  function testHreflangTag() {
+    global $base_url;
+    // User to add language.
+    $admin_user = $this->drupalCreateUser(array('administer languages', 'access administration pages'));
+    $this->drupalLogin($admin_user);
+    // Add predefined language.
+    $edit = array(
+      'predefined_langcode' => 'fr',
+    );
+    $this->drupalPostForm('admin/config/regional/language/add', $edit, 'Add language');
+    $this->drupalGet('user/2');
+    $this->assertRaw('<link href="' . $base_url . '/fr/user/2" rel="alternate" hreflang="fr" />', 'French hreflang found on English page.');
+    $this->assertRaw('<link href="' . $base_url . '/user/2" rel="alternate" hreflang="en" />', 'English hreflang found on English page.');
+    $this->drupalGet('fr/user/2');
+    $this->assertRaw('<link href="' . $base_url . '/fr/user/2" rel="alternate" hreflang="fr" />', 'French hreflang found on French page.');
+    $this->assertRaw('<link href="' . $base_url . '/user/2" rel="alternate" hreflang="en" />', 'English hreflang found on French page.');
+  }
+}
